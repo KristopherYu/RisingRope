3b6b5414b762e82cc69eff53bc1c88c0faa69c2c
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-10-12 16:44:07 -0700

    Added some starting code to layout framework

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-title = "";
+title = "Rising Rope";
 
 description = `
+[Hold]
+  Grapple onto a point
 `;
 
-characters = [];
+characters = [   //Custom Sprites
+  `
+   b
+  byb
+   b
+  `
+];
 
-options = {};
+const G = {   //Data we can call later
+  WIDTH: 90,
+  HEIGHT: 150
+}
+
+options = {
+  theme:'pixel',
+  //seed: 53, //9 is good, 46 is ok, 53 is good
+  //isPlayingBgm: true,
+  viewSize: {x: G.WIDTH, y: G.HEIGHT}
+};
+
+/**
+ * @typedef {{
+ * pos: Vector
+ * }} Hook
+ */
+/**
+ * @type { Hook [] } 
+*/
+let hooks
 
 function update() {
   if (!ticks) {
