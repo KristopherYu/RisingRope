58d519033086af8d3d2b5405b630f9fa2763f8ce
KapeRay <67844957+KapeRay@users.noreply.github.com>
2021-10-12 18:32:59 -0700

    Update main.js
    just made platforms spawn randomly from the top with hooks

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 title = "Rising Rope";
 
 description = `
-[Hold]
-  Hook to a point
+    [Hold]
+  Grapple onto 
+    a point
 `;
 
 characters = [   //Custom Sprites
   `
- b
-byb
- b
+   b
+  byb
+   b
   `
-  ,
-`
-pppppp
-pyppyp
-pyppyp
-pppppp
-pp  pp
-pp  pp
-`
 ];
 
 const G = {   //Data we can call later
@@ -36,30 +28,67 @@ options = {
 
 /**
  * @typedef {{
- * pos: Vector
+ * pos: Vector,
+ * vx: number,
+ * hooklaneIndex: number
  * }} Hook
  */
 /**
  * @type { Hook [] } 
 */
 let hooks
-
-/**
- * @typedef {{
- * pos: Vector
- * }} Player
- */
 let player
+let nextHookTicks;
+let hooklaneSpeeds
+const hooklaneCount = 4;
+const hooklaneWidth = 20;
 
 function update() {
-  if (!ticks) { //Initialize Values/Sprites/Characters
-
-    player = {
-      pos: vec(G.WIDTH / 2, G.HEIGHT/2)
-    }
+  if (!ticks) {
+    let player = vec(50, 80); 
+    hooks = []
+    nextHookTicks = 0;
+    hooklaneSpeeds = times(hooklaneCount, () => 1);
+    
   }
-    //player
-    color("black");
-    char("b", player.pos);
 
+  nextHookTicks--;
+  if (nextHookTicks < 0) {
+    //play("select");
+    const hooklaneIndex = rndi(hooklaneCount);
+    hooks.push({
+      pos: vec(rnd(10, 110), 0),
+      //Messes with the speed
+      vx: -rnd(1, sqrt(difficulty)) * 0.5,
+      hooklaneIndex,
+    });
+    nextHookTicks = rnd(99, 100) / difficulty;
+  }
+
+  color("cyan")
+  rect(0, 0, 5, 5)
+  if(input.isPressed){
+    line(player, input.pos)
+  }
+  
+  remove(hooks, (e) => {
+    e.pos.y -= e.vx * hooklaneSpeeds[e.hooklaneIndex];
+    const hook = char("a", e.pos.x, e.pos.y - 48)
+    color("light_cyan")
+    const platform = box(e.pos, 30, 10)
+    if(input.isJustPressed){
+      if((input.pos.x > e.pos.x - 3  && input.pos.x < e.pos.x + 3) && (input.pos.y > e.pos.y - 51  && input.pos.y < e.pos.y - 45)) {
+        player = vec(e.pos.x, e.pos.y - 48)
+        
+      }
+    }  
+    
+  });
+  
+  
+  
+  function calcX(i) {
+    return i * hooklaneWidth + hooklaneWidth / 2 + 12;
+  }
+  
 }
